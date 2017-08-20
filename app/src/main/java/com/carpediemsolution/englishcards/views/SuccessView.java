5759b18cf4005c3c8af8bfed47c13c1a102f867b
Ulia <batova.j@yandex.ru>
2017-08-20 17:47:09 +0300

    20/08/2017

---
+++ app/src/main/java/com/carpediemsolution/englishcards/views/SuccessView.java
@@ -0,0 +1,14 @@
+package com.carpediemsolution.englishcards.views;
+
+import com.arellomobile.mvp.MvpView;
+
+/**
+ * Created by Юлия on 20.08.2017.
+ */
+
+public interface SuccessView extends MvpView {
+    /**
+     * доработать общее дерево интерфейсов
+     */
+    void showSuccess();
+}
